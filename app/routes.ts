34bc75ee18c148a6a9b56389c7f95a0fe78bae20
mlnchk <danil.mlnchk@gmail.com>
2025-01-07 17:44:51 +0700

    design: move note to layout

--- app/routes.ts
+++ app/routes.ts
@@ -9,6 +9,6 @@ export default [
   layout("./routes/layout.tsx", [
     index("./routes/notes/index.tsx"),
     route("note/:noteId", "./routes/notes/note.tsx"),
+    route("shared/:encodedNote", "./routes/notes/shared.tsx"),
   ]),
-  route("shared/:encodedNote", "./routes/shared.tsx"),
 ] satisfies RouteConfig;
